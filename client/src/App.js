66d3b188d224053d85150cce6ac817ccd96853de
john-roufaeil <john.flouis1@gmail.com>
2023-10-06 17:49:31 +0300

    feat: add sign in and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import * as ReactDOM from "react-dom/client";
+import ButtonAppBar from './components/ButtonAppBar';
+import PatientRegister from './pages/PatientRegister';
+import PharmacistRegister from './pages/PharmacistRegister';
+import SignIn from './pages/SignIn';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/register/patient",
+      element: <PatientRegister />,
+    },
+    {
+      path: "/register/pharmacist",
+      element: <PharmacistRegister />
+    },
+    {
+      path: "/signin",
+      element: <SignIn />,
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
+
     </div>
   );
 }
