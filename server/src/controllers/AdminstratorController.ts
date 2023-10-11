1ee62d66d6161112c7454063244bca8128007e5f
ahmedyasser07 <ahmedtaweel.65@gmail.com>
2023-10-11 19:34:14 +0300

    added validation for all users

--- server/src/controllers/AdminstratorController.ts
+++ server/src/controllers/AdminstratorController.ts
@@ -7,18 +7,24 @@ import patient from "../models/Patient.js";
 const createAdminstrator = async(req:Request, res:Response)=>{
         //add another adminstrator with a set username and password
         // missing authentication part
-        req.body.username = "admin";
-        req.body.passwordHash = "admin";
-        const newAdminstrator = adminstrator
-        .create(req.body)
-        .then((newAdminstrator) => {
-        res.status(200).json(newAdminstrator);
-        })
-        .catch((err) => {
-        console.log("error");
-        res.status(400).json(err);
-        });
+        const entry = adminstrator.find({ 'username': req.body.username }).then((document) => {
+          if (document.length === 0) {
+    
+            const newAdmin = adminstrator
+            .create(req.body)
+            .then((newAdmin) => {
+                res.status(200).json(newAdmin);
+            })
+            .catch((err) => {
+                res.status(400).json(err);
+            });
+
+          }
+          else if (document.length !== 0)
+              res.status(400).send("username taken , please choose another one ");
+      })
 }
+
 const deleteAdmin = async (req: Request, res: Response) => {
     const id = req.params.id;
     const newAdminstrator = adminstrator
