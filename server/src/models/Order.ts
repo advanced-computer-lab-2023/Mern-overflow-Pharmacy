85a9ae0f31eb90d581b71ef6f0ead7b63c3c7090
john-roufaeil <john.flouis1@gmail.com>
2023-10-28 12:07:23 +0200

    feat: update order schema

--- server/src/models/Order.ts
+++ server/src/models/Order.ts
@@ -1,23 +1,29 @@
-import mongoose, { Schema, model } from "mongoose";
+import mongoose, { Date, Schema, model } from "mongoose";
 
 interface CartObject {
-    medicine: typeof mongoose.Types.ObjectId;
-    quantity: number;
-}
-
-interface ICart {
-    patient: typeof mongoose.Types.ObjectId;
-    cartObjects: CartObject[];
+    medName: String;
+    medPrice: Number;
+    medQuantity: Number;
 }
 
 interface IOrder {
     patient: typeof mongoose.Types.ObjectId;
-    status: string;
-    //objects: 
+    status: String;
+    date: Date;
+    total: Number;
+    medicines: CartObject[];
 }
 
 const orderSchema = new Schema<IOrder>({
+    patient: { type: mongoose.Types.ObjectId, required: true, ref: "Patient", unique: true },
     status: { type: String, required: true, lowercase: true, enum: ['pending', 'shipped', 'delievered', 'cancelled'] },
+    date: { type: Date, required: true },
+    total: { type: Number, required: true },
+    medicines: [{
+        medName: { type: String, required: true, },
+        medPrice: { type: Number, required: true, },
+        medQuantity: { type: Number, required: true, },
+    },],
 })
 
 
