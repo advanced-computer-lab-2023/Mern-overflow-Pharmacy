53f80b1a2258e2f06b98bb568fdba6d43c38971c
Mohamed Mohey <mmi9433@gmail.com>
2023-12-10 13:01:41 +0200

    Sales Report fix

---
+++ client/src/pages/admin/AdminViewSalesReport.js
@@ -0,0 +1,13 @@
+import AdminDashboard from "./AdminDashboard";
+import ViewSalesReport from "../../components/ViewComponents/ViewSalesReport";
+
+const AdminViewSalesReport = () => {
+  return (
+    <>
+      <AdminDashboard title="View Sales Report" />
+      <ViewSalesReport />
+    </>
+  );
+};
+
+export default AdminViewSalesReport;
---
+++ client/src/pages/pharmacist/PharmacistViewSalesReport.js
@@ -0,0 +1,13 @@
+import PharmacistDashboard from "./PharmacistDashboard";
+import ViewSalesReport from "../../components/ViewComponents/ViewSalesReport";
+
+const PharmacistViewSalesReport = () => {
+  return (
+    <>
+      <PharmacistDashboard title="View Sales Report" />
+      <ViewSalesReport />
+    </>
+  );
+};
+
+export default PharmacistViewSalesReport;
